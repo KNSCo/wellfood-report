050ff6508065cbbcf64e16a13adc1f0ce0335b2c
junyoung.han <hjy0085@naver.com>
2024-06-04 07:04:29 +0900

    ubi 개발서버 수정

--- UbiService/bin/shutdown.sh
+++ UbiService/bin/shutdown.sh
@@ -10,7 +10,7 @@ GREP_STR=$UBISERVICE_DIR/lib/UbiServer.jar
 #	kill -9 `ps -efx | grep $GREP_STR | grep -v grep | awk '{print $2}'`
 
 if [ `ps -ef | grep $GREP_STR | grep -v grep | awk '{print $2}'` ];then
-	sudo kill -9 `ps -ef | grep $GREP_STR | grep -v grep | awk '{print $2}'`
+	kill -9 `ps -ef | grep $GREP_STR | grep -v grep | awk '{print $2}'`
 	echo ""
 	echo ">>> UbiService has shutdown."
 	echo ""
